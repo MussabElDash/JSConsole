c634b40ada7ee7d0f131139bbb4f3e18a5f10d14
MussabElDash <mussab14@gmail.com>
2014-07-23 05:25:32 +0200

    change to the use of contenteditable

--- JSConsole.js
+++ JSConsole.js
@@ -7,101 +7,65 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 
 		var $ConsoleElement = $("#" + ConsoleElementId).addClass("jsconsole");
 		$ConsoleElement.attr("tabindex", 0);
-		var typing = false;
 
 		$ConsoleElement.on({
-			keypress: function(e){
-				typing = true;
-				var child = $(this).children('.jsconsole_before_cursor');
-				child.text(child.text() + String.fromCharCode(e.which));
-			}, keydown: function(e){
-				typing = true;
-				var child_before = $(this).children('.jsconsole_before_cursor');
-				var child_after = $(this).children('.jsconsole_after_cursor');
+			keydown: function(e){
 				switch(e.which){
-					// backspace
-					case 8:
-						e.preventDefault();
-						child_before.text(child_before.text().slice(0, -1));
-						break;
 					// tab
 					case 9:
 						e.preventDefault();
-						child_before.text(child_before.text() + "\t");
+						$(this).text($(this).text() + "\t");
+						setEndOfContenteditable(this);
 						break;
 					// enter
 					case 13:
 						e.preventDefault();
 						addLine();
 						break;
-					//space
-					case 32:
-						e.preventDefault();
-						child_before.text(child_before.text() + " ");
-						break;
-					//end
-					case 35:
-						e.preventDefault();
-						child_before.text(child_before.text() + child_after.text());
-						child_after.text("");
-						break;
-					//home
-					case 36:
-						e.preventDefault();
-						child_after.text(child_before.text() + child_after.text());
-						child_before.text("");
-						break;
-					// delete
-					case 46:
-						e.preventDefault();
-						child_after.text(child_after.text().slice(1));
-						break;
-					// left
-					case 37:
-						e.preventDefault();
-						var chr = child_before.text().slice(-1);
-						child_before.text(child_before.text().slice(0, -1));
-						child_after.text(chr + child_after.text());
-						break;
-					// right
-					case 39:
-						e.preventDefault();
-						var chr = child_after.text().slice(0, 1);
-						child_after.text(child_after.text().slice(1));
-						child_before.text(child_before.text() + chr);
-						break;
 				}
 			}
-		}, ".jsconsole_input.jsconsole_active");
+		}, ".jsconsole_input.jsconsole_active .jsconsole_line");
 
-		$ConsoleElement.focus(function(){
+		$ConsoleElement.click(function(e){
+			if($(e.target).is(".jsconsole_input:not(.jsconsole_active) .jsconsole_line"))
+				return;
 			var $toBeFocused = $ConsoleElement.
-				children(".jsconsole_input.jsconsole_active");
+				children(".jsconsole_input.jsconsole_active").
+				children(".jsconsole_line");
 			$toBeFocused.focus();
 		});
 
-		var cursorAnimation = function () {
-			if(typing)
-				return;
-			$cursor.animate({
-				opacity: 0
-			}, 'fast', 'swing').animate({
-				opacity: 1
-			}, 'fast', 'swing');
-		}
-
 		var addLine = function(){
 			$ConsoleElement.children(".jsconsole_input.jsconsole_active").
-				removeClass("jsconsole_active").
-				removeAttr("tabindex");
+				removeClass("jsconsole_active").children(".jsconsole_line").
+				removeAttr("contenteditable");
 			var $dir = $("<span class='jsconsole_directory'>JSConsole:~$</span>");
-			var $after = $("<span class='jsconsole_after_cursor'></span>");
-			var $before = $("<span class='jsconsole_before_cursor'></span>");
+			var $line = $("<div class='jsconsole_line' contenteditable></div>");
 			var $active = $("<div class='jsconsole_input jsconsole_active'></div>").
-				attr("tabindex", 0).append($dir).
-				append($before).append($after);
+				append($dir).append($line);
 			$ConsoleElement.append($active);
-			$active.focus();
+			$line.focus();
+		}
+
+		function setEndOfContenteditable(contentEditableElement)
+		{
+			var range,selection;
+			if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+			{
+				range = document.createRange();//Create a range (a range is a like the selection but invisible)
+				range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+				range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+				selection = window.getSelection();//get the selection object (allows you to change selection)
+				selection.removeAllRanges();//remove any selections already made
+				selection.addRange(range);//make the range you have just created the visible selection
+			}
+			else if(document.selection)//IE 8 and lower
+			{ 
+				range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+				range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+				range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+				range.select();//Select the range (make it the visible selection
+			}
 		}
 
 		addLine();
@@ -148,35 +112,13 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 			$style.append(styleText);
 		}
 
-		var before_and_after = function(){
-			var styleText = ".jsconsole_after_cursor, .jsconsole_before_cursor{";
-			styleText += "display:inline;"
-			styleText += "border:none;"
+		var line = function(){
+			var styleText = ".jsconsole_line{";
+			styleText += "display:inline;";
+			styleText += "border:none;";
+			styleText += "outline:0;";
 			styleText += "word-wrap:break-word;";
 			styleText += "white-space:pre-wrap;";
-			styleText += "}";
-			$style.append(styleText);
-		}
-
-		var active_before = function(){
-			var styleText = "@-webkit-keyframes blink-empty{";
-			styleText += "50% {border-left: 1px solid #fff;}";
-			styleText += "}";
-			styleText += "-moz-@keyframes blink-empty{";
-			styleText += "50% {border-left: 1px solid #fff;}";
-			styleText += "}";
-			styleText += "-o-@keyframes blink-empty{";
-			styleText += "50% {border-left: 1px solid #fff;}";
-			styleText += "}";
-			styleText += "@keyframes blink-empty{";
-			styleText += "50% {border-left: 1px solid #fff;}";
-			styleText += "}";
-			styleText += ".jsconsole_input.jsconsole_active .jsconsole_after_cursor{";
-			styleText += "-webkit-animation: blink-empty 1s infinite;";
-			styleText += "-moz-animation: blink-empty 1s infinite;";
-			styleText += "-o-animation: blink-empty 1s infinite;";
-			styleText += "animation: blink-empty 1s infinite;";
-			styleText += "border-left: transparent solid 1px;";
 			styleText += "}";
 			$style.append(styleText);
 		}
@@ -185,8 +127,7 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 		active();
 		cosole();
 		dir();
-		before_and_after();
-		active_before();
+		line();
 		$("head").append($style);
 	}
 
