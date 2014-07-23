3c079a00309460a7edb9fb20c58034e2d625b990
MussabElDash <mussab14@gmail.com>
2014-07-23 04:06:01 +0200

    add some functionality

--- JSConsole.js
+++ JSConsole.js
@@ -1,39 +1,82 @@
 if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery') }
 (function ($, window, undefined) {
 
-	window.JSConsole = function(ConsoleElemntId){
+	window.JSConsole = function(ConsoleElementId){
 		if( !(this instanceof JSConsole) )
-			return new JSConsole(ConsoleElemntId);
+			return new JSConsole(ConsoleElementId);
 
-		$cursor = $("<span id='jsconsole_cursor'>|</span>");
-		var $ConsoleElemnt = $("#" + ConsoleElemntId).addClass("jsconsole");
+		var $ConsoleElement = $("#" + ConsoleElementId).addClass("jsconsole");
+		$ConsoleElement.attr("tabindex", 0);
 		var typing = false;
 
-		$ConsoleElemnt.on({
+		$ConsoleElement.on({
 			keypress: function(e){
 				typing = true;
-				$(this).text($(this).text() + String.fromCharCode(e.which));
+				var child = $(this).children('.jsconsole_before_cursor');
+				child.text(child.text() + String.fromCharCode(e.which));
 			}, keydown: function(e){
 				typing = true;
+				var child_before = $(this).children('.jsconsole_before_cursor');
+				var child_after = $(this).children('.jsconsole_after_cursor');
 				switch(e.which){
+					// backspace
 					case 8:
 						e.preventDefault();
-						$(this).text($(this).text().slice(0, -1));
+						child_before.text(child_before.text().slice(0, -1));
 						break;
+					// tab
+					case 9:
+						e.preventDefault();
+						child_before.text(child_before.text() + "\t");
+						break;
+					// enter
 					case 13:
 						e.preventDefault();
 						addLine();
 						break;
+					//space
+					case 32:
+						e.preventDefault();
+						child_before.text(child_before.text() + " ");
+						break;
+					//end
+					case 35:
+						e.preventDefault();
+						child_before.text(child_before.text() + child_after.text());
+						child_after.text("");
+						break;
+					//home
+					case 36:
+						e.preventDefault();
+						child_after.text(child_before.text() + child_after.text());
+						child_before.text("");
+						break;
+					// delete
+					case 46:
+						e.preventDefault();
+						child_after.text(child_after.text().slice(1));
+						break;
+					// left
+					case 37:
+						e.preventDefault();
+						var chr = child_before.text().slice(-1);
+						child_before.text(child_before.text().slice(0, -1));
+						child_after.text(chr + child_after.text());
+						break;
+					// right
+					case 39:
+						e.preventDefault();
+						var chr = child_after.text().slice(0, 1);
+						child_after.text(child_after.text().slice(1));
+						child_before.text(child_before.text() + chr);
+						break;
 				}
-			}, keyup: function(){
-				setTimeout(function(){
-					typing = false;
-				},1000);
 			}
 		}, ".jsconsole_input.jsconsole_active");
 
-		$ConsoleElemnt.click(function(){
-			var $toBeFocused = $("#" + ConsoleElemntId + " .jsconsole_input.jsconsole_active");
+		$ConsoleElement.focus(function(){
+			var $toBeFocused = $ConsoleElement.
+				children(".jsconsole_input.jsconsole_active");
 			$toBeFocused.focus();
 		});
 
@@ -48,18 +91,20 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 		}
 
 		var addLine = function(){
-			$("#" + ConsoleElemntId + " .jsconsole_input.jsconsole_active")
-				.removeClass("jsconsole_active")
-				.removeAttr("tabindex");
-			var $dir = $("<div class='jsconsole_directory'>JSConsole:~$ </div>");
-			var $active = $("<div class='jsconsole_input jsconsole_active'></div>");
-			$active.attr("tabindex", -1);
-			$cursor.detach();
-			$ConsoleElemnt.append($dir).append($active).append($cursor).click();
+			$ConsoleElement.children(".jsconsole_input.jsconsole_active").
+				removeClass("jsconsole_active").
+				removeAttr("tabindex");
+			var $dir = $("<span class='jsconsole_directory'>JSConsole:~$</span>");
+			var $after = $("<span class='jsconsole_after_cursor'></span>");
+			var $before = $("<span class='jsconsole_before_cursor'></span>");
+			var $active = $("<div class='jsconsole_input jsconsole_active'></div>").
+				attr("tabindex", 0).append($dir).
+				append($before).append($after);
+			$ConsoleElement.append($active);
+			$active.focus();
 		}
 
 		addLine();
-		setInterval (cursorAnimation, 1000);
 	}
 
 	var styles = function(){
@@ -70,7 +115,6 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 			var styleText = ".jsconsole_input{";
 			styleText += "outline:none;";
 			styleText += "display:block;";
-			styleText += "height:16px;"
 			styleText += "}";
 			$style.append(styleText);
 		}
@@ -86,7 +130,6 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 			var styleText = ".jsconsole{";
 			styleText += "background-color:#2f0b24;";
 			styleText += "color:white;";
-			styleText += "white-space:pre-wrap;";
 			styleText += "height:100%;";
 			styleText += "width:100%;";
 			styleText += "overflow:auto;";
@@ -96,16 +139,44 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 			$style.append(styleText);
 		}
 
-		var cursor = function(){
-			var styleText = "#jsconsole_cursor{";
-			styleText += "font-weight:900;"
+		var dir = function(){
+			var styleText = ".jsconsole_directory{";
+			styleText += "padding-right:4px;";
+			styleText += "padding-left:4px;";
+			styleText += "display:inline;"
 			styleText += "}";
 			$style.append(styleText);
 		}
 
-		var dir = function(){
-			var styleText = ".jsconsole_directory{";
+		var before_and_after = function(){
+			var styleText = ".jsconsole_after_cursor, .jsconsole_before_cursor{";
 			styleText += "display:inline;"
+			styleText += "border:none;"
+			styleText += "word-wrap:break-word;";
+			styleText += "white-space:pre-wrap;";
+			styleText += "}";
+			$style.append(styleText);
+		}
+
+		var active_before = function(){
+			var styleText = "@-webkit-keyframes blink-empty{";
+			styleText += "50% {border-left: 1px solid #fff;}";
+			styleText += "}";
+			styleText += "-moz-@keyframes blink-empty{";
+			styleText += "50% {border-left: 1px solid #fff;}";
+			styleText += "}";
+			styleText += "-o-@keyframes blink-empty{";
+			styleText += "50% {border-left: 1px solid #fff;}";
+			styleText += "}";
+			styleText += "@keyframes blink-empty{";
+			styleText += "50% {border-left: 1px solid #fff;}";
+			styleText += "}";
+			styleText += ".jsconsole_input.jsconsole_active .jsconsole_after_cursor{";
+			styleText += "-webkit-animation: blink-empty 1s infinite;";
+			styleText += "-moz-animation: blink-empty 1s infinite;";
+			styleText += "-o-animation: blink-empty 1s infinite;";
+			styleText += "animation: blink-empty 1s infinite;";
+			styleText += "border-left: transparent solid 1px;";
 			styleText += "}";
 			$style.append(styleText);
 		}
@@ -113,8 +184,9 @@ if (typeof jQuery === 'undefined') { throw new Error('JSConsole requires jQuery'
 		input();
 		active();
 		cosole();
-		cursor();
 		dir();
+		before_and_after();
+		active_before();
 		$("head").append($style);
 	}
 
